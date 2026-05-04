ac93b76267a3a7333604f38744296d65530f7d4c
Juan Martínez <jmartinez.graphic@gmail.com>
2026-05-04 16:58:47 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let j = 0; j < pages.length; j++) {
-      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+      if (this.dataset.page === pages[j].dataset.page) {
         pages[j].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
